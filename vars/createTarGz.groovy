1e70ae13c0d051fb91b8e3d8ebc240fdf8ca920e
David THIBAU <david.thibau@gmail.com>
2023-11-03 10:26:14 +0100

    tarGz

---
+++ vars/createTarGz.groovy
@@ -0,0 +1,16 @@
+// my-shared-library/vars/createTarGz.groovy
+
+def call(Map config) {
+    def sourceDir = config.sourceDir
+    def extensions = config.extensions
+    def outputDir = config.outputDir
+
+    echo "Création d'une distribution tar.gz à partir de $sourceDir avec les extensions : ${extensions.join(', ')}"
+
+    // Créez une chaîne de filtres pour rechercher plusieurs extensions
+    def extensionFilter = extensions.collect { ext -> "--include=\"*.$ext\"" }.join(' ')
+
+    // Utilisez des commandes shell pour créer le tar.gz
+    sh "mkdir -p $outputDir"
+    sh "tar -czvf $outputDir/my_distribution.tar.gz -C $sourceDir $extensionFilter ."
+}
